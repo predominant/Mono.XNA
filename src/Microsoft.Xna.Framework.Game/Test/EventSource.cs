43a73a46ca57d34543376bd8a73e7c7f304d49c7
stuart.carnie <stuart.carnie@0d1dca7f-3429-0410-b39c-edaab0c29397>
2007-05-01 17:18:58 +0000

    Resolved duplicated text
    git-svn-id: http://monoxna.googlecode.com/svn/trunk@76 0d1dca7f-3429-0410-b39c-edaab0c29397

--- src/Microsoft.Xna.Framework.Game/Test/EventSource.cs
+++ src/Microsoft.Xna.Framework.Game/Test/EventSource.cs
@@ -17,22 +17,3 @@ namespace Microsoft.Xna.Framework.Test
 }
 
 #endif
-#if NUNITTESTS
-
-namespace Microsoft.Xna.Framework.Test
-{
-    public abstract class EventSource
-    {
-        Game _game;
-
-        public abstract void Run();
-
-        public Game Game
-        {
-            get { return _game; }
-            set { _game = value; }
-        }
-    }
-}
-
-#endif
