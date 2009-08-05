9f8828e73e32f64a47b14bc5f41232ff2a566073
lavima <lavima@0d1dca7f-3429-0410-b39c-edaab0c29397>
2009-08-05 16:27:17 +0000

    changed fileencoding
    git-svn-id: http://monoxna.googlecode.com/svn/trunk@168 0d1dca7f-3429-0410-b39c-edaab0c29397

---
+++ tests/TestServiceProvider.cs
@@ -0,0 +1,16 @@
+
+using System;
+using Microsoft.Xna.Framework;
+
+namespace Tests
+{
+	
+	
+	public class TestServiceProvider : IServiceProvider
+	{
+		
+		public TestServiceProvider()
+		{
+		}
+	}
+}
