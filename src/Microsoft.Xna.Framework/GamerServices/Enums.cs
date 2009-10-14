bc1ed05931ef737c8190887de75f9f904c5b5dd4
timpambor <timpambor@0d1dca7f-3429-0410-b39c-edaab0c29397>
2009-10-14 18:46:45 +0000

    git-svn-id: http://monoxna.googlecode.com/svn/trunk@242 0d1dca7f-3429-0410-b39c-edaab0c29397

--- src/Microsoft.Xna.Framework/GamerServices/Enums.cs
+++ src/Microsoft.Xna.Framework/GamerServices/Enums.cs
@@ -9,4 +9,118 @@
         Medium = 1,
         High = 2,
     }
+
+    public enum GameDifficulty
+    {
+        Easy,
+        Normal,
+        Hard
+    }
+
+    public enum GamerPresenceMode
+    {
+        None,
+        SinglePlayer,
+        Multiplayer,
+        LocalCoOp,
+        LocalVersus,
+        OnlineCoOp,
+        OnlineVersus,
+        VersusComputer,
+        Stage,
+        Level,
+        CoOpStage,
+        CoOpLevel,
+        ArcadeMode,
+        CampaignMode,
+        ChallengeMode,
+        ExplorationMode,
+        PracticeMode,
+        PuzzleMode,
+        ScenarioMode,
+        StoryMode,
+        SurvivalMode,
+        TutorialMode,
+        DifficultyEasy,
+        DifficultyMedium,
+        DifficultyHard,
+        DifficultyExtreme,
+        Score,
+        VersusScore,
+        Winning,
+        Losing,
+        ScoreIsTied,
+        Outnumbered,
+        OnARoll,
+        InCombat,
+        BattlingBoss,
+        TimeAttack,
+        TryingForRecord,
+        FreePlay,
+        WastingTime,
+        StuckOnAHardBit,
+        NearlyFinished,
+        LookingForGames,
+        WaitingForPlayers,
+        WaitingInLobby,
+        SettingUpMatch,
+        PlayingWithFriends,
+        AtMenu,
+        StartingGame,
+        Paused,
+        GameOver,
+        WonTheGame,
+        ConfiguringSettings,
+        CustomizingPlayer,
+        EditingLevel,
+        InGameStore,
+        WatchingCutscene,
+        WatchingCredits,
+        PlayingMinigame,
+        FoundSecret,
+        CornflowerBlue
+    }
+
+    public enum GamerPrivilegeSetting
+    {
+        Blocked,
+        FriendsOnly,
+        Everyone
+    }
+    public enum GamerZone
+    {
+        Unknown,
+        Recreation,
+        Pro,
+        Family,
+        Underground
+    }
+
+    public enum RacingCameraAngle
+    {
+        Back,
+        Front,
+        Inside
+    }
+    public enum MessageBoxIcon
+    {
+        None,
+        Error,
+        Warning,
+        Alert
+    }
+
+    public enum NotificationPosition
+    {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        CenterLeft,
+        Center,
+        CenterRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight
+    }
+
 }
