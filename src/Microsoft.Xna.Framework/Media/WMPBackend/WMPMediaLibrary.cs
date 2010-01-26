62bd7b54d45cd2842203f7b81cd15f63a7eb52c1
timpambor <timpambor@0d1dca7f-3429-0410-b39c-edaab0c29397>
2010-01-26 19:02:02 +0000

    git-svn-id: http://monoxna.googlecode.com/svn/trunk@288 0d1dca7f-3429-0410-b39c-edaab0c29397

--- src/Microsoft.Xna.Framework/Media/WMPBackend/WMPMediaLibrary.cs
+++ src/Microsoft.Xna.Framework/Media/WMPBackend/WMPMediaLibrary.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Interop.WMPLib;
 
 namespace Microsoft.Xna.Framework.Media
 {
     internal class WMPMediaLibrary : IMediaLibrary
     {
+        IWMPCore wmp = (IWMPCore)(new WindowsMediaPlayerClass());
 
         MediaSource Mediasource;
 
@@ -39,22 +41,87 @@ namespace Microsoft.Xna.Framework.Media
 
         public SongCollection GetSongs()
         {
-            throw new NotImplementedException();
+            IWMPMediaCollection media = wmp.mediaCollection;
+            IWMPPlaylist playlist = media.getAll();
+            List<Song> songlist = new List<Song>();
+
+            for (int i = 0; i < playlist.count; i++)
+            {
+                IWMPMedia temp = playlist[i];
+                if (temp.getItemInfo("MediaType") == "audio")
+                {
+                    int wmprating;
+                    int rating;
+                    int tracknumber;
+                    int playcount;
+                    bool IsProtected;
+                    if (!int.TryParse(temp.getItemInfo("UserRating"), out wmprating))
+                    {
+                        throw new Exception();
+                    }
+                    if (wmprating >= 87) rating = 10;
+                    else if (wmprating >= 63) rating = 8;
+                    else if (wmprating >= 38) rating = 6;
+                    else if (wmprating >= 13) rating = 4;
+                    else if (wmprating >= 1) rating = 2;
+                    else rating = 0;
+                    if (!int.TryParse(temp.getItemInfo("WM/TrackNumber"), out tracknumber))
+                    {
+                        tracknumber = 0;
+                    }
+                    if (!int.TryParse(temp.getItemInfo("PlayCount"), out playcount))
+                    {
+                        throw new Exception();
+                    }
+                    if (!bool.TryParse(temp.getItemInfo("Is_Protected"), out IsProtected))
+                    {
+                        throw new Exception();
+                    }
+                    TimeSpan duration = new TimeSpan(0, 0, 0, 0, (int)(temp.duration * 1000.0));
+                    songlist.Add(new Song(temp.name, duration, rating, tracknumber, temp, mediasource));
+                }
+            }
+            SongCollection songs = new SongCollection(songlist);
+            return songs;
         }
 
         public int getSong_PlayCount(object handle)
         {
-            throw new NotImplementedException();
+            int playcount;
+            if (!int.TryParse(((IWMPMedia)(handle)).getItemInfo("PlayCount"), out playcount))
+            {
+                throw new Exception();
+            }
+            return playcount;
         }
 
         public bool getSong_IsProtected(object handle)
         {
-            throw new NotImplementedException();
+            bool IsProtected;
+            if (!bool.TryParse(((IWMPMedia)(handle)).getItemInfo("Is_Protected"), out IsProtected))
+            {
+                throw new Exception();
+            }
+            return IsProtected;
         }
 
         public AlbumCollection GetAlbums()
         {
             throw new NotImplementedException();
+            IWMPMediaCollection media = wmp.mediaCollection;
+            IWMPPlaylist playlist = media.getAll();
+            List<Album> albumlist = new List<Album>();
+
+            for (int i = 0; i < playlist.count; i++)
+            {
+                IWMPMedia temp = playlist[i];
+                if (temp.getItemInfo("MediaType") == "audio")
+                {
+                    string albumname = temp.getItemInfo("Album");
+                }
+            }
+            AlbumCollection albums = new AlbumCollection(albumlist);
+            return albums;
         }
 
         public PictureAlbum GetRootPictureAlbum()
